c776e753c67624c1c0b53a2e839aeff4ab498612
VSUS <152472758+VSUSnator@users.noreply.github.com>
2025-01-14 08:13:38 +0800

    Add files via upload

---
+++ bin/Map/MapLoader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tanki.Map
+{
+    public class MapLoader
+    {
+        private readonly List<string> mapFiles;
+        private int currentMapIndex;
+
+        public MapLoader(List<string> mapFiles)
+        {
+            this.mapFiles = mapFiles;
+            currentMapIndex = 0;
+        }
+
+        public string GetNextMap()
+        {
+            if (currentMapIndex < mapFiles.Count)
+            {
+                return mapFiles[currentMapIndex++];
+            }
+            return null; // Если карты закончились
+        }
+    }
+}
